68cfe389d9021553bfcf89d3c6d0ffea4668bcd6
Pathompong Sangkajit <p.inhumba@gmail.com>
2019-05-22 20:12:17 +0700

    Styling Radio

--- pug/script/master-script.js
+++ pug/script/master-script.js
@@ -3,3 +3,4 @@
 <script async="async" src="assets/vendors/popper/popper.min.js"></script>
 <script src="assets/vendors/bootstrap/bootstrap.min.js"></script>
 <script src="assets/vendors/bootstrap/bootstrap-select.min.js"></script>
+<script src="assets/script/main.min.js"></script>

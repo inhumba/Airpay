c59169ef8c7ba89fbebfd6755c3162c962fa9552
Pathompong Sangkajit <p.inhumba@gmail.com>
2019-05-23 14:35:07 +0700

    Include Datepicker

--- pug/script/master-script.js
+++ pug/script/master-script.js
@@ -3,4 +3,5 @@
 <script async="async" src="assets/vendors/popper/popper.min.js"></script>
 <script src="assets/vendors/bootstrap/bootstrap.min.js"></script>
 <script src="assets/vendors/bootstrap/bootstrap-select.min.js"></script>
+<script src="assets/vendors/bootstrap/bootstrap-datepicker.min.js"></script>
 <script src="assets/script/main.min.js"></script>
--- src/script/main.js
+++ src/script/main.js
@@ -3,5 +3,11 @@ $(function () {
 		mobile: true
 	});
 
+	$('.datepicker').datepicker({
+		autoclose: true,
+		format: 'dd/mm/yyyy',
+		startDate: '1d'
+	});
+
 	$('[data-toggle="tooltip"]').tooltip();
 });

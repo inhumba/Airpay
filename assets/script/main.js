6356bfb50de66d1146a34733666a2130441d6007
Pathompong Sangkajit <p.inhumba@gmail.com>
2019-05-24 08:30:22 +0700

    Change compress "main.js" file to uncompress

---
+++ assets/script/main.js
@@ -0,0 +1,16 @@
+$(function () {
+	$('.selectpicker').selectpicker({
+		mobile: true
+	});
+
+	$('.datepicker').datepicker({
+		autoclose: true,
+		disableTouchKeyboard: true,
+		format: 'dd/mm/yy',
+		startDate: '1d'
+	});
+
+	$('#label-checkbox-accept').on('click', function() {
+		$('#modal').modal('show');
+	});
+});
